75b2e1f3ddf2ba3de55f8aa375cfa15bd7df1054
riyan <146079241+ryanraditya@users.noreply.github.com>
2024-03-08 20:30:31 +0700

    Add files via upload

---
+++ proses_form.php
@@ -0,0 +1,18 @@
+<?php
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$jenis_kelamin = $_POST['jenis_kelamin'];
+$prodi = $_POST['prodi'];
+$keahlian = $_POST['keahlian'];
+$domisili = $_POST['domisili'];
+$email = $_POST['email'];
+?>
+
+<h1> Terima kasih sudah mendaftar di it club</h1>
+<h5> Nama lengkap :<?php echo $nama ?></h5>
+<h5> Nim : <?php echo $nim ?></h5>
+<h5> Jenis Kelamin : <?php echo $jenis_kelamin ?></h5>
+<h5> Prodi : <?php echo $prodi ?></h5>
+<h5> Keahlian : <?php echo $keahlian ?></h5>
+<h5> Domisili : <?php echo $domisili ?></h5>
+<h5> Email : <?php echo $email ?></h5>
